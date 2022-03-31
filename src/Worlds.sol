71f9c2ec4f10f59541f77eea0aa6340317ca5596
jtardioli <joshua.tardioli@gmail.com>
2022-03-30 20:41:26 -0400

    rename stuff

---
+++ src/Worlds.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.13;
+
+import "lib/ERC721A.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "solmate/utils/ReentrancyGuard.sol";
+
+error InsufficientFunds();
+error ExceedsMaxSupply();
+error BeforeSaleStart();
+error FailedTransfer();
+error ExceedsWhitelistAllowance();
+error UnmatchedLength();
+
+
+/// @author https://github.com/jtardioli
+contract Worlds {
+
+}
